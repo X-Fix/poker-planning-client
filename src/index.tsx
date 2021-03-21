e15f1c3ed2e99b2651406221759f7a19caeee378
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-03-21 16:14:12 +0100

    Add emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from '@emotion/styled';
+
 import printMe from './print';
 
 const Body: React.FC = () => {
+  const Body = styled.div`
+    color: red;
+  `;
   return (
-    <div>
+    <Body>
       Hello webpack
       <button onClick={printMe}>Click me and check the console</button>
-    </div>
+    </Body>
   );
 };
 
