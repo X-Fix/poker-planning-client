4b1c291369c173a38e2127eb66d7c4c17b030586
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-03-21 18:23:58 +0100

    Add emotion babel plugin, streamline webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,89 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const babelRule = {
-  test: /\.ts(x?)$/,
-  include: /src/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            modules: 'auto',
-            targets: 'defaults',
-          },
-        ],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
-      ],
-      cacheDirectory: true,
+module.exports = (env) => {
+  console.log(env.NODE_ENV);
+  const isProd = env.NODE_ENV === 'production';
+
+  const emotionBabelPlugin = [
+    '@emotion',
+    {
+      sourceMap: !isProd,
+      autoLabel: 'always',
+      cssPropOptimization: true,
     },
-  },
-};
+  ];
 
-const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  meta: {
-    charset: 'UTF-8',
-    viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
-  },
-  title: 'React App',
-});
+  const babelRule = {
+    test: /\.ts(x?)$/,
+    include: /src/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        plugins: [emotionBabelPlugin],
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: 'auto',
+              targets: 'defaults',
+            },
+          ],
+          '@babel/preset-react',
+          '@babel/preset-typescript',
+        ],
+        cacheDirectory: true,
+      },
+    },
+  };
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: {
-    symlinks: false,
-  },
-  module: {
-    rules: [babelRule],
-  },
-  plugins: [htmlWebpackPlugin],
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
+  const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    meta: {
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+    },
+    title: 'React App',
+  });
+
+  return {
+    mode: env.NODE_ENV,
+    devtool: !isProd && 'inline-source-map',
+    devServer: isProd
+      ? {}
+      : {
+          // Only log to browser console if there's an error
+          clientLogLevel: 'error',
+          // Open in browser once server has started
+          open: false,
+          // Show an overlay in the browser on compile error
+          overlay: true,
+          // Show progress details in the terminal
+          progress: true,
+        },
+    entry: './src/index.tsx',
+    output: {
+      filename: `bundle.${isProd ? '[contenthash]' : ''}.js`,
+      path: path.resolve(__dirname, isProd ? 'dist' : 'serve'),
+      clean: true,
+    },
+    resolve: {
+      symlinks: false,
+    },
+    module: {
+      rules: [babelRule],
+    },
+    plugins: [htmlWebpackPlugin],
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    stats: {
+      all: false,
+      assets: isProd,
+      builtAt: isProd,
+      errors: true,
+      excludeModules: isProd,
+      timings: true,
+      warnings: true,
+    },
+  };
 };
