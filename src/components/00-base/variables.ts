721c69efedee25f260e51be203bb6526ce7935e7
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-03-27 15:01:38 +0100

    refactor: change import style

--- src/components/00-base/variables.ts
+++ src/components/00-base/variables.ts
@@ -1,51 +1,55 @@
 /* Color (I know it's the wrong spelling but it's also the correct CSS property)
  * ------------------------------------------------------------------
  */
-export const colorNeutral900 = '#000000';
-export const colorNeutral700 = '#616161';
-export const colorNeutral300 = '#90a4ae';
-export const colorNeutral200 = '#b0bec5';
-export const colorNeutral100 = '#f5f5f5';
-export const colorNeutral50 = '#fafafa';
-export const colorNeutral0 = '#ffffff';
+export const color = {
+  blue900: '#17429f',
+  blue800: '#1d60be',
+  blue700: '#2591f1',
+  blue50: '#e3f1fd',
+  blue25: '#f1f8fe',
 
-export const colorBlue900 = '#17429f';
-export const colorBlue800 = '#1d60be';
-export const colorBlue700 = '#2591f1';
-export const colorBlue50 = '#e3f1fd';
-export const colorBlue25 = '#f1f8fe';
+  iconBlack: '#000000',
+  iconBlue: '#1d60be',
+  iconGreen: '#00914c',
+  iconGrey: '#90a4ae',
+  iconWhite: '#ffffff',
 
-export const colorGreen700 = '#00914c';
-export const colorGreen100 = '#e3f6ec';
+  neutral900: '#000000',
+  neutral700: '#616161',
+  neutral300: '#90a4ae',
+  neutral200: '#b0bec5',
+  neutral100: '#f5f5f5',
+  neutral50: '#fafafa',
+  neutral0: '#ffffff',
 
-export const colorIconBlack = colorNeutral900;
-export const colorIconBlue = colorBlue800;
-export const colorIconGreen = colorGreen700;
-export const colorIconGrey = colorNeutral300;
-export const colorIconWhite = colorNeutral0;
+  green700: '#00914c',
+  green100: '#e3f6ec',
+};
 
 /* Shadows
  * ------------------------------------------------------------------
  */
 const shadow = 'rgba(0, 0, 0, 0.25)';
 
-export const shadowButtonPrimary = '2px 3px 1px rgba(0, 0, 0, 0.35)';
-export const shadowButtonSecondary = `2px 3px 2px ${shadow}`;
-export const shadowButtonTertiary = '2px 3px 1px rgba(0, 0, 0, 0.375)';
+export const shadows = {
+  buttonPrimary: '2px 3px 1px rgba(0, 0, 0, 0.35)',
+  buttonSecondary: `2px 3px 2px ${shadow}`,
+  buttonTertiary: '2px 3px 1px rgba(0, 0, 0, 0.375)',
 
-export const shadowPrimary = `2px 3px 2px ${shadow}`;
-export const shadowSecondary = `0px 2px 8px ${shadow}`;
-export const shadowTertiary = `inset 1px 1px 2px ${shadow}`;
+  primary: `2px 3px 2px ${shadow}`,
+  secondary: `0px 2px 8px ${shadow}`,
+  tertiary: `inset 1px 1px 2px ${shadow}`,
 
-export const shadowFooter = `0 -2px 4px ${shadow}`;
-export const shadowHeader = `0 2px 4px ${shadow}`;
-export const shadowForm = `0 2px 8px ${shadow}`;
+  footer: `0 -2px 4px ${shadow}`,
+  header: `0 2px 4px ${shadow}`,
+  form: `0 2px 8px ${shadow}`,
 
-/* There is overlap between the chat-bubble shadow and the triangle's 'shadow', causing a darker
- * shadow where they overlap. To avoid this we make an exception in this instance, and use a solid
- * colour for the box-shadow. The value below is the solid representation of rgba(0, 0, 0, 0.25)
- */
-export const shadowChatBubble = '#bfbfbf';
+  /* There is overlap between the chat-bubble shadow and the triangle's 'shadow', causing a darker
+   * shadow where they overlap. To avoid this we make an exception in this instance, and use a solid
+   * colour for the box-shadow. The value below is the solid representation of rgba(0, 0, 0, 0.25)
+   */
+  chatBubble: '#bfbfbf',
+};
 
 /* Typography
  * ------------------------------------------------------------------
@@ -53,8 +57,10 @@ export const shadowChatBubble = '#bfbfbf';
 
 export const fontFamily = '"Questrial", sans-serif';
 
-export const fontSizeBody = '1.125rem';
-export const fontSizeTitle = '1.5rem';
-export const fontSizeHeadline = '2rem';
-export const fontSizeCardSmall = '3rem';
-export const fontSizeCardLarge = '4rem';
+export const fontSize = {
+  body: '1.125rem',
+  title: '1.5rem',
+  headline: '2rem',
+  cardSmall: '3rem',
+  cardLarge: '4rem',
+};
