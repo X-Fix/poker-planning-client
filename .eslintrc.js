0864a804621a7beef08f225635414e84b176e3cb
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-04-03 19:13:20 +0200

    feat: add testcafe

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  extends: ['airbnb-typescript', 'eslint-config-prettier'],
+  extends: [
+    'airbnb-typescript',
+    'eslint-config-prettier',
+    'plugin:testcafe/recommended',
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
+  plugins: ['testcafe'],
   rules: {
     'react/jsx-props-no-spreading': [
       true,
